d8bf0f7c50ca3304c74be8cfd11c3878f8e47201
Ruy Armando <ruyarmando@gmail.com>
2020-11-05 16:52:23 -0300

    Revisado package

--- SearchBarApp.Android/MainActivity.cs
+++ SearchBarApp.Android/MainActivity.cs
@@ -7,7 +7,11 @@ using Xamarin.Forms.Platform.Android;
 
 namespace SearchBarApp.Droid
 {
-    [Activity(Label = "SearchBarApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "SearchBarApp"
+        , Icon = "@mipmap/icon"
+        , Theme = "@style/MainTheme"
+        , MainLauncher = true
+        , ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
